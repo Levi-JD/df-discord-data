d21fd6c9272db9820960c738f4d3ae6382e0a823
lewi <levi_jd@icloud.com>
2025-10-31 02:05:34 +0100

    database slqite3

--- dbcreate.py
+++ dbcreate.py
@@ -1,18 +1,39 @@
 import sqlite3
 
-with sqlite3.connect('user_tokens_count.db') as connection:
+connection = sqlite3.connect("tokens.sqlite")
+cursor = connection.cursor()
 
-    cursor = connection.cursor()
+cursor.executescript("""
+        PRAGMA journal_mode=WAL;
+        PRAGMA synchronous=NORMAL;
+        PRAGMA temp_store=MEMORY;
+        PRAGMA mmap_size=30000000000; -- ok if ignored on your system
+    """)
 
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS token_ids (
-        token_id INTEGER PRIMARY KEY AUTOINCREMENT,
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS tokens (
+        id INTEGER PRIMARY KEY,
         token TEXT UNIQUE
     );
-    '''
 
-    # Execute the SQL command
-    cursor.execute(create_table_query)
+    CREATE TABLE IF NOT EXISTS user_tokens_count (
+        user_id TEXT,
+        token_id INTEGER,
+        count INTEGER NOT NULL,
+        PRIMARY KEY (user_id, token_id),
+        FOREIGN KEY (token_id) REFERENCES tokens(id),
+        FOREIGN KEY (user_id)  REFERENCES users(id)
+    );
+    
+    CREATE TABLE IF NOT EXISTS users (
+        id TEXT PRIMARY KEY,
+        user_name TEXT
+    );
+    CREATE INDEX IF NOT EXISTS idx_tokens_token ON tokens(token);
+    CREATE INDEX IF NOT EXISTS idx_utc_user ON user_tokens_count(user_id);
+    CREATE INDEX IF NOT EXISTS idx_utc_token ON user_tokens_count(token_id);
+"""
+cursor.executescript(create_table_query)
 
-    # Commit the changes
-    connection.commit()
+connection.commit()
+connection.close()
